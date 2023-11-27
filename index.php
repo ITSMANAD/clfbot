9a90f4d8a5142bfe1103f9a70a292b16d7dea20b
ITSMANAD <itsmanadi@gmail.com>
2023-11-27 17:31:44 +0330

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$telegram = new Telegram('6463000943:AAHvswPJRHUhwkB2dnoLapokgc5LucCamnQ');
+
+$chat_id = $telegram->ChatID();
+$content = array('chat_id' => $chat_id, 'text' => 'Test');
+$telegram->sendMessage($content);
