3e4b4f52289b6c516f959ef2f8e4da2eb30c0fa1
ITSMANAD <itsmanadi@gmail.com>
2023-11-27 18:32:20 +0330

    1

--- index.php
+++ index.php
@@ -1,35 +1,8 @@
 <?php
-class MyTmpTelegramBot
-{
-    const BOT_TOKEN = "6463000943:AAHvswPJRHUhwkB2dnoLapokgc5LucCamnQ";
-    const TELEGRAM_API_URL = "https://api.telegram.org/bot";
+include 'Telegram.php';
 
-    public $url;
+$telegram = new Telegram('6463000943:AAHvswPJRHUhwkB2dnoLapokgc5LucCamnQ');
 
-    public function __construct()
-    {
-        $this->url = SELF::TELEGRAM_API_URL . SELF::BOT_TOKEN;
-    }
-
-    private function runScript($method)
-    {
-        return file_get_contents($this->url . '/'. $method);
-    }
-
-    public function getUpdates()
-    {
-        return $this->runScript('getupdates');
-    }
-
-    public function sendMessage($chatId, $text)
-    {
-        $url = "sendmessage?text=$text&chat_id=$chatId";
-        return $this->runScript($url);
-    }
-}
-
-$obj = new MyTmpTelegramBot();
-$updatesJson = $obj->getUpdates();
-$updatesJson2Array = json_decode($updatesJson, true);
-$chatId = $updatesJson2Array['result'][0]['message']['chat']['id'];
-$obj->sendMessage($chatId, 'Hi');
+$chat_id = $telegram->ChatID();
+$content = array('chat_id' => $chat_id, 'text' => 'Test');
+$telegram->sendMessage($content);
